f046c1e244bc227b652923abbea522a029db00c1
Jaeu Jeong <wodndb@gmail.com>
2024-12-30 16:45:19 +0900

    Add i18n utils based on next-intl

---
+++ src/i18n/routing.ts
@@ -0,0 +1,17 @@
+import { defineRouting } from "next-intl/routing";
+import { createNavigation } from "next-intl/navigation";
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ["en", "ko", "jp"],
+
+  // Used when no locale matches
+  defaultLocale: "ko",
+});
+
+export type Locale = (typeof routing.locales)[number];
+
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing);
